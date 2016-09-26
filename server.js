c0cc27ccaf0144db6ffcb01c0d5f58896a77729e
Priyapp <priyapwarrier@gmail.com>
2016-09-26 23:47:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,21 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
+app.get('/resouce1', function (req, res) {
+      res.send('Requested resource1');
+});
+
+app.get('/resouce2', function (req, res) {
+  res.send('Requested resource1');
+});
+
+app.get('/resouce3', function (req, res) {
+  res.send('Requested resource3');
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
