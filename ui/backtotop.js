572dc6a24768589fc92d25541362deedbadc79bd
Priyapp <priyapwarrier@gmail.com>
2016-10-23 22:14:33 +0530

    [imad-console] Updates ui/backtotop.js

--- ui/backtotop.js
+++ ui/backtotop.js
@@ -9,6 +9,6 @@ $(function() {
 
     $('#totop').click(function(event) {
             event.preventDefault();
-        $('html, body').animate({scrollTop:0},800);
+        $('html, body').animate({scrollTop : 0}, 800);
     });
 });
